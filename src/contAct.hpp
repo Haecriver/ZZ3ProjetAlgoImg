0ee32c4d61908c6d127b233131cc2313660f3edf
Haecriver <you@example.com>
2016-12-10 19:15:04 +0100

    Modif non fructeuses ...

--- src/contAct.hpp
+++ src/contAct.hpp
@@ -77,15 +77,15 @@ void InitLevelSet(CImg<>* imgIn, int x0,int y0, int r)
 void Propagate(
 	CImg<> imgIn,
 	CImg<>* LevelSet,
-	int nbIter = 20000,						// Nombre d'itération
-	float alpha = 0.05, 					// Pas temporel
-	float beta = 0.8, 						// Pondération du terme de propagation
-	float gamma = 0.5,
-	float delta_t = 2.0,					// Pondération du terme d'advection 
-	float ballon = -0.01) 				// Force ballon
+	int nbIter,						// Nombre d'itération
+	float alpha, 					// Pondération du terme de propagation
+	float beta, 						// Pondération du terme d'advection
+	float gamma,
+	float delta_t,					// Pas temporel
+	float ballon) 				// Force ballon
 {
 	// Calcul des gradients spaciaux régularisés de l'image
-	const CImgList<> gradImg = imgIn.get_gradient("xy", 4 /* Filtre qui régularise */);
+	const CImgList<> gradImg = imgIn.get_channel(0).get_gradient("xy", 4 /* Filtre qui régularise */);
 	const CImg<> squareGradImgNorme = (gradImg[0].get_sqr() + gradImg[1].get_sqr());
 	
 	// Calculer le module de chaque vecteur
@@ -94,27 +94,49 @@ void Propagate(
 /******************************************************************************/
 // Affichage HornSchunck
 {
-	float color = 500; unsigned int sampling = 8; float factor = 40; int quiver_type = 0; float opacity = 0.5;
+	//float color = 500; unsigned int sampling = 8; float factor = 40; int quiver_type = 0; float opacity = 0.5;
 
-	CImg<> imageHS = imgIn.get_slice(0).draw_quiver(imgInHS, &color, opacity, sampling, factor, quiver_type);
-    CImgDisplay resHS_disp(imageHS, "Horn et Schunck");
+	//CImg<> imageHS = imgIn.get_slice(0).draw_quiver(imgInHS, &color, opacity, sampling, factor, quiver_type);
+    //CImgDisplay resHS_disp(imageHS, "Horn et Schunck");
 
-	while (!resHS_disp.is_closed()) { resHS_disp.wait(); }
+	//while (!resHS_disp.is_closed()) { resHS_disp.wait(); }
 }
 /******************************************************************************/
 
 	// Calcul du module des vecteurs vitesse
-	const CImg<> imgInVitesse = (imgInHS.get_channel(0).get_sqr() + imgInHS.get_channel(1).get_sqr()).get_sqrt();
+	const CImg<> imgInVitesse = ((imgInHS.get_channel(0).get_sqr() + imgInHS.get_channel(1).get_sqr()).get_sqrt());
+
+	// Normalement pas util
+	const CImgList<> gradimgInVitesse = imgInVitesse.get_gradient("xy",0);
+	const CImg<> squareGradImgVitess = (gradimgInVitesse[0].get_sqr() + gradimgInVitesse[1].get_sqr());
+	
+	
 	
 	// Calcul de la fonction d'arrêt g du modèle géodésique
 	CImg<> g(imgIn.width(), imgIn.height(), 1, 1);
 	cimg_forXY(g, x, y)
 	{
-		g(x, y) = ( 1.0 / double(1.0 + squareGradImgNorme(x, y)) ) + ballon + (imgInVitesse(x, y) * gamma);
+		//g(x, y) = ( 1.0 / double(1.0 + squareGradImgNorme(x, y)*0.05 + imgInVitesse(x,y)*10 ) ) + ballon;
+		g(x, y) = ( 1.0 / double(1.0 + squareGradImgNorme(x, y) ) ) + ballon - imgInVitesse(x,y)*gamma;
 	}
 	
-	const CImgList<> gradG = g.get_gradient("xy");
+	/////////////// AFFICHAGE DEBUG ///////////////
 	
+	CImgDisplay resg_disp(g, "gedodesique");
+	CImgDisplay resVitesse_disp(imgInVitesse, "Vitesse");
+	CImgDisplay resGradVitesse_disp(squareGradImgVitess, "Gradient Vitesse");
+	CImgDisplay ressquareGradImgNorme_disp(squareGradImgNorme, "Norme grad");
+	while (!resVitesse_disp.is_closed() && 
+	!resGradVitesse_disp.is_closed() && 
+	!ressquareGradImgNorme_disp.is_closed() &&
+	!resg_disp.is_closed()) 
+	{
+		resVitesse_disp.wait(); 
+	}
+	
+	
+	const CImgList<> gradG = g.get_gradient("xy");
+
 	// Note : p = plus / m = moins
 	for (int i = 0 ; i < nbIter ; ++i)
 	{
@@ -125,6 +147,8 @@ void Propagate(
 				
 		cimg_forXY(*LevelSet, x, y)
 		{
+			const float moduleVitesseCur = imgInVitesse(x, y) * gamma;
+		
 			// Valeurs des dérivées de LevelSet
 			const float& Dxm = Dm[0](x, y);
 			const float& Dxp = Dp[0](x, y);
@@ -149,9 +173,13 @@ void Propagate(
 											+ minDxp0 * minDxp0
 											+ maxDym0 * maxDym0
 											+ minDyp0 * minDyp0);
+			const float nablam = std::sqrt(	maxDxp0 * maxDxp0
+											+ minDxm0 * minDxm0
+											+ maxDyp0 * maxDyp0
+											+ minDym0 * minDym0);
 		
 			// Calcul de la vitesse de propagation
-			const float Fprop = - MAX(g(x, y), 0) * nablap;
+			const float Fprop = - (MAX(g(x, y), 0) * nablap + MIN(g(x, y), 0) * nablam);
 
 			// Calcul de la vitesse d'advection
 			const float& u = gradG[0](x, y);
@@ -171,12 +199,12 @@ void Propagate(
 
 void executeContourActif(	
 	CImg<> img, 
-	int nbIter = 2000,
-	float alpha = 0.05, 
-	float beta = 0.5, 
-	float gamma = 0.8,
-	float delta_t = 2.0, 
-	float ballon = -0.01)
+	int nbIter,
+	float alpha, 
+	float beta, 
+	float gamma,
+	float delta_t, 
+	float ballon)
 {
 	// Définition d'un contour initial circulaire
 	int x0 = img.width()/2;
--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,12 @@
 
 int main(int argc, char* argv[]){
 
-	int nbIter = cimg_option("-n", 2000, "Nombre d'itérations");
-	float alpha = cimg_option("-a", 0.05, "Coefficient Alpha");
-	float beta = cimg_option("-b", 0.5, "Coefficient Beta");
-	float gamma = cimg_option("-g", 0.8, "Coefficient Gamma");
+	int nbIter = cimg_option("-n", 10000, "Nombre d'itérations");
+	float alpha = cimg_option("-a", 0.05, "Coefficient Alpha (propagation)");
+	float beta = cimg_option("-b", 0.5, "Coefficient Beta (advection)");
+	float gamma = cimg_option("-g", 0.5, "Coefficient Gamma (vitesse)");
+	float delta_t = cimg_option("-dt", 2.0, "Pas temporel");
+	float ballon = cimg_option("-ballon", -0.01, "Force ballon");
 
 	const char *firstImage = cimg_option("-i1", "../rsc/taxi1.bmp", "Premiere image");
 	const char *secondImage = cimg_option("-i2", "../rsc/taxi2.bmp", "Seconde image");
@@ -17,7 +19,7 @@ int main(int argc, char* argv[]){
 	CImg<> seq = CImg<>(firstImage).channel(0);
 	seq.append(CImg<>(secondImage).channel(0),'z');
 
-	executeContourActif(seq, nbIter, alpha, beta, gamma);
+	executeContourActif(seq, nbIter, alpha, beta, gamma, delta_t, ballon);
 
 	return EXIT_SUCCESS;
 }
