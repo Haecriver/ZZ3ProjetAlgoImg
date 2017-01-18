cfa1fa901d2a8dc626d9f228dd209e3732daee33
Haecriver <you@example.com>
2017-01-18 19:36:30 +0100

    Essaie de se faire l'image taxi mais non en fait

--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,30 @@ int main(int argc, char* argv[]){
 
 /*
 	VALEUR carre1.png carre2.png
-	./mosuofe -a 0.3 -t 0.05
+	./mosuofe -img1 ../rsc/carre1.bmp -img2 ../rsc/carre2.bmp -a 0.3 -t 0.05
 	
 	VALEUR carreBois1.png carreBois2.png
-	./mosuofe -a 0.5 -t 0.05 -g 0.3 -b 0.3
+	./mosuofe -img1 ../rsc/carreBois1.bmp -img2 ../rsc/carreBois2.bmp -a 0.5 -t 0.05 -g 0.3 -b 0.3
+
+	VALEUR taxi1.png taxi2.png NE MARCHE PAS TROP TROP BIEN QUOI
+	./mosuofe -a 0.1 -t 0.2 -g 1.0 -b 0.5
 
 */
+
+	const char* image_path_1 = cimg_option("-img1", "../rsc/taxi1.bmp", "Image 1");
+	const char* image_path_2 = cimg_option("-img2", "../rsc/taxi2.bmp", "Image 1");
 
 	int nbIter = cimg_option("-n", 10000, "Nombre d'itérations");
 	float alpha = cimg_option("-a", 0.1, "Coefficient Alpha (propagation)");
 	float beta = cimg_option("-b", 0.5, "Coefficient Beta (advection)");
 	float gamma = cimg_option("-g", 0.5, "Coefficient Gamma (vitesse)");
+	float theta = cimg_option("-t", 0.5, "Coefficient theta (geodesique)");
 	float delta_t = cimg_option("-dt", 2.0, "Pas temporel");
 	float ballon = cimg_option("-ballon", -0.01, "Force ballon");
-	float theta = cimg_option("-t", 0.5, "Force theta");
 
-	std::cout << "teta ;" << gamma << std::endl;
 
-	const char *firstImage = cimg_option("-i1", "../rsc/carreBois1.bmp", "Premiere image");
-	const char *secondImage = cimg_option("-i2", "../rsc/carreBois2.bmp", "Seconde image");
+	const char *firstImage = cimg_option("-i1", image_path_1, "Premiere image");
+	const char *secondImage = cimg_option("-i2", image_path_2, "Seconde image");
 
 	CImg<> seq = CImg<>(firstImage).channel(0);
 	seq.append(CImg<>(secondImage).channel(0),'z');
